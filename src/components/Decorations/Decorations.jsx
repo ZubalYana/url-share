533bfb70c2745113c1e005e3fda86528385b3b76
MarkivNazar <nazarmn2008@gmail.com>
2025-05-28 00:10:02 +0300

    stylized right page decoration

--- src/components/Decorations/Decorations.jsx
+++ src/components/Decorations/Decorations.jsx
@@ -5,9 +5,13 @@ export default function Decorations() {
   return (
     <>
       <div>
-     <img className='absolute top-[-85px] left-[17px] h-[309px] z-10 spin-slow' src="/DecorationsLeft1.svg" alt="gear1" />
-<img className='absolute top-[190px] left-[-58px] h-[212px] z-10 spin-medium' src="/DecorationsLeft2.svg" alt="gear2" />
+     <img className='absolute top-[-85px] left-[17px] h-[309px] z-10 spin-slow1' src="/DecorationsLeft1.svg" alt="gear1" />
+<img className='absolute top-[188px] left-[-58px] h-[212px] z-10 spin-medium1' src="/DecorationsLeft2.svg" alt="gear2" />
 
+
+
+ <img className='absolute bottom-[196px] right-[-64px] spin-slow2' src="/DecorationsRight1.svg" alt="gear1" />
+<img className='absolute bottom-[-45px] right-[-10px] spin-medium2' src="/DecorationsRight2.svg" alt="gear2" />
 
 
 
