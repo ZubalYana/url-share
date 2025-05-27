7123566077c90a00c5f5e008c29b6db57d78c68a
MarkivNazar <nazarmn2008@gmail.com>
2025-05-27 22:55:12 +0300

    connected header to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Header from './components/Header/Header'
 import UriActions from './components/UriActions/UriActions'
 function App() {
 
-    // git merge main щоб працював tailwind в інших гілках 
+    // git merge main в гілці щоб працював tailwind в інших гілках 
 
   return (
     <>
+
+      <Header />
 
       <UriActions />
     </>
