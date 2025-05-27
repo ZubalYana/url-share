fbf12983b5fa08d00d7ded11d8a9156ff153f0e2
MarkivNazar <nazarmn2008@gmail.com>
2025-05-27 09:39:23 +0300

    instruction

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import UriActions from './components/UriActions/UriActions'
 function App() {
 
+    // git merge main щоб працював tailwind в інших гілках 
+
   return (
     <>
       <UriActions />
