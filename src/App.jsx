6be1bfe4de6a00742bf9e67f2d308899a49f48c1
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-05-27 08:10:19 +0300

    create the uri sharing section and style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-
+import UriSharing from './components/uriSharing/uriSharing'
 function App() {
 
   return (
     <>
+      <UriSharing />
     </>
   )
 }
---
+++ src/components/uriSharing/uriSharing.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './UriSharing.css'
+import { TextField, Button } from '@mui/material'
+
+export default function UriSharing() {
+    return (
+        <div className='uriLogicSection'>
+            <h3 className='uriLogicSection_title'>Share your URI</h3>
+            <TextField
+                label="Enter a URI to share..."
+                variant="outlined"
+                fullWidth
+                sx={{
+                    backgroundColor: '#f9f9f9',
+                    borderRadius: '8px',
+                    '& .MuiOutlinedInput-root': {
+                        '& fieldset': {
+                            borderColor: '#aaa',
+                        },
+                        '&:hover fieldset': {
+                            borderColor: '#888',
+                        },
+                        '&.Mui-focused fieldset': {
+                            borderColor: '#673ab7',
+                        },
+                    },
+                    '& .MuiInputLabel-root': {
+                        fontSize: '16px',
+                    },
+                }}
+            />
+            <Button
+                variant="contained"
+                fullWidth
+                sx={{
+                    backgroundColor: '#3255D5',
+                    color: '#fff',
+                    borderRadius: '10px',
+                    textTransform: 'none',
+                    marginTop: '20px',
+                    width: '270px',
+                    height: '55px',
+                    fontSize: '16px',
+                    fontWeight: '600',
+                    textTransform: 'uppercase',
+                    '&:hover': {
+                        backgroundColor: '#2C48AA',
+                    },
+                }}
+            >
+                Get a code
+            </Button>
+        </div>
+    )
+}
