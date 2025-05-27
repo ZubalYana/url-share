fc598b4ecc868e2523a33aba9ee29e31e7fbc8a2
MarkivNazar <nazarmn2008@gmail.com>
2025-05-28 00:12:54 +0300

    connected the decoration to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header/Header'
 import UriActions from './components/UriActions/UriActions'
+import Decorations from './components/Decorations/Decorations'
 function App() {
 
     // git merge main в гілці щоб працював tailwind в інших гілках 
@@ -11,6 +12,8 @@ function App() {
       <Header />
 
       <UriActions />
+
+      <Decorations />
     </>
   )
 }
