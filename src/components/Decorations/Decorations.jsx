90f889479b5f5972b154f0780f36d693e1bd25ba
MarkivNazar <nazarmn2008@gmail.com>
2025-05-27 23:36:12 +0300

    created and stylized the top left page decoration

---
+++ src/components/Decorations/Decorations.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Decorations.css'
+
+export default function Decorations() {
+  return (
+    <>
+      <div>
+     <img className='absolute top-[-85px] left-[17px] h-[309px] z-10 spin-slow' src="/DecorationsLeft1.svg" alt="gear1" />
+<img className='absolute top-[190px] left-[-58px] h-[212px] z-10 spin-medium' src="/DecorationsLeft2.svg" alt="gear2" />
+
+
+
+
+
+      </div>
+    </>
+  )
+}
