5a8bab320f43e1f6a1361916752de7a08f85d4fe
MarkivNazar <nazarmn2008@gmail.com>
2025-05-30 21:22:35 +0300

    added the ability to navigate by url

--- src/components/BoxUriActions/MiniCompUriActions/UriGettingComp/UriDisplay/UriDisplay.jsx
+++ src/components/BoxUriActions/MiniCompUriActions/UriGettingComp/UriDisplay/UriDisplay.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 export default function UriDisplay({ uri }) {
     return (
         <div style={{ paddingTop: '15px', fontWeight: '600', fontSize: '16px', color: '#333' }}>
-            Your URI: <span style={{ color: '#3255D5' }}>{uri}</span>
+            Your URI:{" "}
+            <a 
+                href={uri} 
+                style={{ color: '#3255D5', cursor: 'pointer', textDecoration: 'none' }}
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                {uri}
+            </a>
         </div>
     );
 }
