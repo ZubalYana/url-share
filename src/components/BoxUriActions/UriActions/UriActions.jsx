1df8858b0af805d40123632fee9731b57376c6f3
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-06-05 11:50:33 +0300

    flexboxes fix

--- src/components/BoxUriActions/UriActions/UriActions.jsx
+++ src/components/BoxUriActions/UriActions/UriActions.jsx
@@ -3,7 +3,7 @@ import UriSharing from '../uriSharing/uriSharing'
 import UriGetting from '../UriGetting/UriGetting'
 export default function UriActions() {
     return (
-        <div className='uriActions w-full h-fit flex flex-col justify-center items-center gap-[20px] md:flex-row md:gap-[120px]'>
+        <div className='uriActions w-full h-fit flex flex-col justify-center items-center gap-[20px] z-10 relative md:flex-row md:gap-[60px] lg:gap-[120px]'>
             <UriSharing />
             <UriGetting />
         </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Header() {
   return (
 
-    <div className='flex justify-between pt-4 md:pt-[50px] w-full '>
+    <div className='flex justify-between pt-4 md:pt-7 lg:pt-[50px] w-full z-10 relative '>
       <div className='w-0 md:w-[25%]'></div>
       <div className='w-full p-4 md:w-[50%] md:p-0'>
-        <h1 className='text-[24px] font-semibold text-center md:text-[38px]'>URI-SHARE</h1>
-        <p className='text-center text-[12px] font-regular md:text-[18px]'>A simple <span className='text-[#1E47DA] cursor-pointer'> solution </span>  to a common problem — <span className='text-[#1E47DA] cursor-pointer'>easily</span> share URLs and links across devices instantly.</p>
+        <h1 className='text-[24px] font-semibold text-center md:text-[32px] lg:text-[38px]'>URI-SHARE</h1>
+        <p className='text-center text-[12px] font-regular md:text-[16px] lg:text-[18px]'>A simple <span className='text-[#1E47DA] cursor-pointer'> solution </span>  to a common problem — <span className='text-[#1E47DA] cursor-pointer'>easily</span> share URLs and links across devices instantly.</p>
       </div>
       <div className='w-0 hidden justify-center gap-[10px] md:w-[25%] md:flex'>
         <img className='cursor-pointer w-[30px] h-[30px]' src="/avatar.svg" alt="avatar.svg" />
