f4cdbb29568201cbd50b84a81f62bedf5640784c
MarkivNazar <nazarmn2008@gmail.com>
2025-06-05 23:30:47 +0300

    added a function to go to registration

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
@@ -10,7 +11,9 @@ export default function Header() {
         <p className='text-center text-[12px] font-regular md:text-[16px] lg:text-[18px]'>A simple <span className='text-[#1E47DA] cursor-pointer'> solution </span>  to a common problem — <span className='text-[#1E47DA] cursor-pointer'>easily</span> share URLs and links across devices instantly.</p>
       </div>
       <div className='w-0 hidden justify-center gap-[10px] md:w-[25%] md:flex'>
-        <img className='cursor-pointer w-[30px] h-[30px]' src="/avatar.svg" alt="avatar.svg" />
+      <Link to="/auth">
+  <img className='cursor-pointer w-[30px] h-[30px]' src="/avatar.svg" alt="avatar.svg" />
+</Link>
         <p className='font-regular text-[#1E1E1E] text-[18px]'>Guest mode</p>
       </div>
       <img className='cursor-pointer w-[25px] h-[25px] absolute top-4 right-4 md:hidden' src="/avatar.svg" alt="avatar" />
