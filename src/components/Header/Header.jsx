6ec8182e65f11284b360084327f35c3fc9a48da4
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-06-06 05:57:45 +0300

    display the actuall username in the header once logged in

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
+  const user = JSON.parse(localStorage.getItem('user'))
   return (
-
     <div className='flex justify-between pt-4 md:pt-7 lg:pt-[50px] w-full z-10 relative '>
       <div className='w-0 md:w-[25%]'></div>
       <div className='w-full p-4 md:w-[50%] md:p-0'>
         <h1 className='text-[24px] font-semibold text-center md:text-[32px] lg:text-[38px]'>URI-SHARE</h1>
         <p className='text-center text-[12px] font-regular md:text-[16px] lg:text-[18px]'>A simple <span className='text-[#1E47DA] cursor-pointer'> solution </span>  to a common problem — <span className='text-[#1E47DA] cursor-pointer'>easily</span> share URLs and links across devices instantly.</p>
       </div>
-      <div className='w-0 hidden justify-center gap-[10px] md:w-[25%] md:flex'>
+      <div className='w-0 hidden justify-end pr-12 gap-[10px] md:w-[25%] md:flex'>
         <Link to="/auth">
           <img className='cursor-pointer w-[30px] h-[30px]' src="/avatar.svg" alt="avatar.svg" />
         </Link>
         <Link to="/auth">
-          <p className='font-regular text-[#1E1E1E] text-[18px]'>Guest mode</p>
+          <p className='font-regular text-[#1E1E1E] text-[18px]'>
+            {user?.name ? user.name : 'Guest mode'}
+          </p>
         </Link>
       </div>
       <Link to="/auth">
